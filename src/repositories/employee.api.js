e2d8649ad7bdae4ad7aeb22b7915f9f7724913d2
ian <gabionian7@gmail.com>
2022-02-22 10:13:36 +0800

    22/2022

--- src/repositories/employee.api.js
+++ src/repositories/employee.api.js
@@ -9,8 +9,8 @@ export const GetAllEmployeesSort = () => axios.get(`user/getAllUser`)
 export const DeleteEmployee = (id) => axios.delete(`${resource}/${id}`)
 export const UpdateEmployee = (id, payload) => axios.put(`${resource}/${id}`, payload)
 export const GetEmployeeInfo = (id) => axios.get(`${resource}/get_user/${id}`)
-export const addEmployee = (data) => axios.post('store', data)
-export const getEmployee = () => axios.get('users/search/' + this.form.search)
+export const addEmployee = (data) => axios.post(`${resource}`, data)
+    // export const getEmployee = () => axios.get('users/search/' + this.form.search)
 
 //roles
 export const GetAllRoles = () => axios.get(`/roles`)
